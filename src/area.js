67a0352885b66970fd571d1eec031d4383e93123
Alexa <alexa.fazio04@gmail.com>
2019-10-18 21:57:36 -0400

    finished exercise 1-5

--- src/area.js
+++ src/area.js
@@ -1,15 +1,15 @@
 
 const readlineSync = require("readline-sync");
 
-const IN_TO_MM = 25.4;
-let width_in = Number(readlineSync.question("Width: "));
+const INCHES_TO_MM = 25.4;
+let width_in = Number(readlineSync.question("\nWidth: "));
 let length_in = Number(readlineSync.question("Length: "));
 
-let width_mm = width_in * IN_TO_MM;
-let length_mm = length_in * IN_TO_MM;
+let width_mm = width_in * INCHES_TO_MM;
+let length_mm = length_in * INCHES_TO_MM;
 let area = width_mm * length_mm;
+area = area.toFixed(2);
+area = area.toLocaleString();
 
-width_in = String(width_in);
-length_in = String(length_in);
+console.log("\nA(n) " + width_in + "-by-" + length_in + "-inch sheet of paper has an area of " + area + " square millimeter(s).\n" );
 
-console.log("\nA(n) " + width_in + "-by-" + length_in + "-inch sheet of paper has an area of " + area + " square millimeter(s)." );
--- src/hello-user.js
+++ src/hello-user.js
@@ -2,4 +2,4 @@
 const readlineSync = require("readline-sync");
 
 const USER_NAME = readlineSync.question("Hi! What's your name?\n");
-console.log("\nHello, " + USER_NAME + "!");
+console.log("\nHello, " + USER_NAME + "!\n");
